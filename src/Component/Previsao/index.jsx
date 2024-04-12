64db00529b704ee51c58e9c9cfb1329f9b66fcb6
alissonoliveira1 <alissonoliveira201339@gmail.com>
2024-04-11 21:20:03 -0300

    progress 5%

--- src/Component/Previsao/index.jsx
+++ src/Component/Previsao/index.jsx
@@ -1,38 +1,32 @@
 
 import { useState, useEffect } from "react"
-import axios  from "axios"
+import  axios  from "axios"
 
 export default function Previsao(){
 const [previsaoDate, setPrevisaoDate] = useState(null)
 
-useEffect(() => {
+useEffect(() =>{
     const response = async () =>{
-  try{
-const dados =  await axios.get("http://api.openweathermap.org/data/2.5/forecast", {
-        params: {
+try{
+    const date = await axios.get('http://api.openweathermap.org/data/2.5/forecast?',{
+        params:{
             appid:'8f2acecfda4778898f5147d3c193c77f',
             q:'salvador,br',
+           
         }
     })
-
-    setPrevisaoDate(dados.data)
-  }
-  catch{
-
-  }
-    
+setPrevisaoDate(date.data)
+}
+catch{
+console.error('error api')
+}
 
     }
-   
-response()
-}, [])
-
-
+    response()
+},[])
 console.log(previsaoDate)
-
-    return (
-   
-            <div>
+    return(
+        <div>
         {previsaoDate.list.map((forecast, index) => (
           <div key={index}>
             <p>Data e hora: {forecast.dt_txt}</p>
@@ -40,8 +34,6 @@ console.log(previsaoDate)
             <p>Descrição: {forecast.weather[0].description}</p>
           </div>
         ))}
-        </div>
+      </div>
     )
-       
-    
 }
